fbe7dd1f770b5e3c881c7addd1a0b93a5033aecd
Ricardo Alencar <ricfilho0007@gmail.com>
2024-11-19 10:33:40 -0300

    feat: create task controller

---
+++ api/src/http/controllers/create-task.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { makeCreateTasksService } from "../../services/factories/make-create-task-service";
+
+export async function createTask(c: any) {
+  const CreateTaskSchema = z.object({
+    userId: z.string(),
+    name: z.string(),
+    description: z.string(),
+  });
+
+  const { userId, name, description } = await c.req.json();
+
+  try {
+    CreateTaskSchema.parse({ userId, name, description });
+
+    const createTaskService = makeCreateTasksService();
+
+    const { task } = await createTaskService.execute({ userId, name, description });
+
+    return c.json({ task });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return c.status(400).json({ error: "Invalid data", details: error.errors });
+    }
+
+    return c.status(500).json({ error: "Internal server error" });
+  }
+}
--- api/src/http/routes/tasks-routes.ts
+++ api/src/http/routes/tasks-routes.ts
@@ -1,10 +1,15 @@
 import { Hono } from 'hono';
 import { getAllTasks } from '../controllers/get-all-tasks';
+import { createTask } from '../controllers/create-task';
 
 const tasksRouter = new Hono();
 
 tasksRouter.get('/:userId', async (c) => {
   return getAllTasks(c);
+});
+
+tasksRouter.post('/create', async (c) => {
+  return createTask(c);
 });
 
 export default tasksRouter;
