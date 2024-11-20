015dff1123bbec017cbd7908e7e4d69b4ef06933
Ricardo Alencar <ricfilho0007@gmail.com>
2024-11-20 08:39:12 -0300

    test: get task by id service test (#1)

---
+++ api/src/services/tasks-service/get-task-by-id-service.test.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { MockTasksRepository } from "../../repositories/mock/mock-tasks-repository";
+import { GetTaskByIdService } from "./get-task-by-id-service";
+import { object } from "zod";
+
+let tasksRepository: MockTasksRepository;
+let sut: GetTaskByIdService;
+
+describe('Get Task By Id Service', () => {
+    beforeEach(() => {
+        tasksRepository = new MockTasksRepository();
+        sut = new GetTaskByIdService(tasksRepository);
+    });
+
+    // criação de um usuário para ter algum registro no banco de dados de memória
+    const createTask = async (userId: string) => {
+        const task = await tasksRepository.create({
+            id: "my-task-id",
+            name: "nova task",
+            description: "descrição da nova task",
+            user: { connect: { id: userId } },
+        });
+        console.log(task); // Log para garantir que a tarefa foi criada corretamente
+        return task;
+    };
+
+
+    it('should be able to get a task by id', async () => {
+        const task = await createTask('user-id');
+
+        const { task: fetchedTask } = await sut.execute({ id: task.id });
+
+        console.log(task); // Log para verificar o que é retornado
+
+        expect(fetchedTask).toHaveProperty('id');
+        expect(fetchedTask).toHaveProperty('name');
+        expect(fetchedTask).toHaveProperty('description');
+        expect(fetchedTask).toHaveProperty('status');
+        expect(fetchedTask).toHaveProperty('created_at');
+        expect(fetchedTask?.userId).toBe('user-id');
+    });
+
+});
