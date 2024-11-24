d6227ff1ff79e6fe77754631366438fa257a166d
Ricardo Alencar <78752259+ricsjs@users.noreply.github.com>
2024-11-24 11:22:26 -0300

    enable cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { cors } from 'hono/cors'
+import { cors } from 'hono/cors';
 import tasksRouter from './http/routes/tasks-routes';
 import usersRouter from './http/routes/users-routes';
 
@@ -9,21 +9,19 @@ app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
-app.use('/*', cors())
 app.use(
   '/*',
   cors({
-    origin: 'http://example.com',
-    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    origin: 'https://desafio-bem-te-h9c43zm78-ricardo-alencars-projects.vercel.app',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type', 'Authorization'],
     allowMethods: ['POST', 'GET', 'OPTIONS'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
     maxAge: 600,
     credentials: true,
   })
-)
+);
 
 app.route('/tasks', tasksRouter);
-
 app.route('/users', usersRouter);
 
 export default app;
