22e4118d1ad44eff5dca42d7c578f2d15d690292
Ricardo Alencar <78752259+ricsjs@users.noreply.github.com>
2024-11-24 11:17:37 -0300

    change api url

--- web/app/services/api.ts
+++ web/app/services/api.ts
@@ -4,7 +4,8 @@ import { parseCookies } from "nookies";
 const cookies = parseCookies();
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://desafio-bem-te-vi.onrender.com/",
+  // baseURL: "http://localhost:3000/",
   headers: {
     Authorization: `Bearer ${cookies["btv.token"]}`,
   },
