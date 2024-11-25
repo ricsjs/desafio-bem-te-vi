474aec3f510ac48f47d2bd22150c18a8b800891e
Ricardo Alencar <ricfilho0007@gmail.com>
2024-11-25 11:54:40 -0300

    docs: inclusion of the relational model, application execution documentation and api documentation with swagger

--- api/src/http/routes/users-routes.ts
+++ api/src/http/routes/users-routes.ts
@@ -12,20 +12,4 @@ usersRouter.post('/login', async (c) => {
   return userAuthenticate(c);
 });
 
-// tasksRouter.get('/task/:id', async (c) => {
-//   return getTaskById(c);
-// })
-
-// tasksRouter.post('/create', async (c) => {
-//   return createTask(c);
-// });
-
-// tasksRouter.put('/update/:id', async (c) => {
-//   return updateTask(c);
-// });
-
-// tasksRouter.delete('/delete/:id', async (c) => {
-//   return deleteTask(c);
-// });
-
 export default usersRouter;
--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,19 @@ import { cors } from 'hono/cors';
 import tasksRouter from './http/routes/tasks-routes';
 import usersRouter from './http/routes/users-routes';
 
+import { swaggerUI } from '@hono/swagger-ui';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
 const app = new Hono();
+
+app.get('/swagger.json', (c) => {
+  const filePath = resolve(__dirname, './swagger.json');
+  const fileContent = readFileSync(filePath, 'utf-8');
+  return c.json(JSON.parse(fileContent));
+});
+
+app.get('/ui', swaggerUI({ url: '/swagger.json' }));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
@@ -12,7 +24,7 @@ app.get('/', (c) => {
 app.use(
   '/*',
   cors({
-    origin: 'https://desafio-bem-te-h9c43zm78-ricardo-alencars-projects.vercel.app',
+    origin: '*',
     allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type', 'Authorization'],
     allowMethods: ['POST', 'GET', 'OPTIONS'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
